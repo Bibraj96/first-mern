9c94b5a76cd5b6d038e2a1a6881f62a61541e79c
bibraj96 <blendi.ibraj96@gmail.com>
2020-12-16 13:33:36 -0500

    remove arbitray comment

--- routes/auth.js
+++ routes/auth.js
@@ -4,14 +4,14 @@ const router = express.Router()
 // @route     GET api/auth
 // @desc      Get logged in user
 // @access    Private
-router.get('/', (req, res) => { // '/' pertains to api/users
+router.get('/', (req, res) => { 
   res.send('Get logged in user')
 })
 
 // @route     POST api/auth
 // @desc      Auth user and get token
 // @access    Public
-router.get('/', (req, res) => { // '/' pertains to api/users
+router.get('/', (req, res) => { 
   res.send('Log in user')
 })
 
