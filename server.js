10a0a49845c21243ffd73f971ae1489fa8dce8b5
bibraj96 <blendi.ibraj96@gmail.com>
2020-12-16 13:24:10 -0500

    use routes from route folders

--- server.js
+++ server.js
@@ -2,14 +2,17 @@
 // can't use import syntax without babel or typescript
 // which is why we're using common js below
 // The code below allows us to create routes
-const express = require('express');
+const express = require('express')
 
-const app = express();
+const app = express()
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
 // This boots up our server
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
 
-// ROUTES
+// Define Routes
+app.use('/api/users', require('./routes/users'))
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/contacts', require('./routes/contacts'))
 app.get('/', (req, res) => res.json({ msg: 'Welcome to my MERN API' }))
