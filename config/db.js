4a268ae03e03c27a53012c5412269c57b57f4ef5
bibraj96 <blendi.ibraj96@gmail.com>
2020-12-16 14:11:37 -0500

    refactor connection with async await

--- config/db.js
+++ config/db.js
@@ -2,18 +2,32 @@ const mongoose = require('mongoose')
 const config = require('config')
 const db = config.get('mongoURI')
 
-const connectDB = () => {
-  mongoose.connect(db, {
-    userNewUrlParser: true,
-    useCreateIndex: true,
-    userFindAndModify: false
-    // above 3 lines prevent warnings in console
-  })
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => {
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      userNewUrlParser: true,
+      useCreateIndex: true,
+      userFindAndModify: false
+      // above 3 lines prevent warnings in console
+    })
+    console.log('MongoDB Connected...')
+  } catch (err) {
     console.error(err.message)
     process.exit(1)
-  })
+  }
 }
+// Same as above without async/await
+// const connectDB = () => {
+  // mongoose.connect(db, {
+  //   userNewUrlParser: true,
+  //   useCreateIndex: true,
+  //   userFindAndModify: false
+  //   // above 3 lines prevent warnings in console
+  // })
+  // .then(() => console.log('MongoDB Connected'))
+  // .catch(err => {
+  //   console.error(err.message)
+  //   process.exit(1)
+// })
 
 module.exports = connectDB
