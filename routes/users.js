b7203c4e13af8129d733bfb30f482918de2854ca
bibraj96 <blendi.ibraj96@gmail.com>
2020-12-17 11:38:04 -0500

    use bcrypt to salt pass and save it to database

--- routes/users.js
+++ routes/users.js
@@ -33,7 +33,11 @@ router.post('/', [
       password
     })
 
+    const salt = await bcrypt.genSalt(10)
+    
+    user.password = await bcrypt.hash(password, salt)
 
+    await user.save()
 
   } catch(err) {
 
