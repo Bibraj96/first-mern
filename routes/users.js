ba7b4f8498d9b2f86f154fc26aee491f1f93ec5b
bibraj96 <blendi.ibraj96@gmail.com>
2020-12-17 11:12:38 -0500

    bring in express validator and validate name

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const { check, validatorResult } = require('express-validator/check')
 
 const User = require('../models/User')
 
 // @route     POST api/users
 // @desc      Register a user
 // @access    Public
-router.post('/', (req, res) => { // '/' pertains to api/users
+router.post('/', [
+  check('name', 'name is required'.not().isEmpty())
+], (req, res) => { // '/' pertains to api/users
   res.send(req.body) // gives us data sent to the route (in this case: email, pass, and name)
 })
 
