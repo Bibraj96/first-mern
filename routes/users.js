f0dc183b9d5fa5e634ac1dfc0ca357b6ab766058
bibraj96 <blendi.ibraj96@gmail.com>
2020-12-17 11:49:32 -0500

    import bcrypt

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
 const { check, validationResult } = require('express-validator/check')
 
 const User = require('../models/User')
