6f5e96a17f6cfdd85829f7632f4b7ca015a983a7
bibraj96 <blendi.ibraj96@gmail.com>
2020-12-17 14:44:58 -0500

    verify token if there is one and send error message if there is not

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,4 +3,23 @@ const jwt = require('jsonwebtoken')
 const config = require('config')
 
 // when we're dealing with middleware functions, you need to call next which essentially says "move on to the next piece of middleware"
-module.exports = function(req, res, next) {}
+module.exports = function(req, res, next) {
+  // Get token from header, if there is one
+  const token = req.header('x-auth-token') // Key to token in header 
+
+  // If there isn't, send error message with 401 status
+  if(!token) {
+    return req.status(401).json({ msg: 'No token, authorization denied' })
+  }
+
+  // If there is a token, take it out, verify it, pull out payload 
+  try{
+    const decoded = jwt.verify(token, config.get('jwtSecret'))
+
+    // Set user from payload to req.user so we'll have access to it in the route
+    req.user = decoded.user
+    next()
+  } catch(err) {
+    res.status(401).json({ msg: 'Token is not valid' })
+  }
+}
