75ef8369a594c7c68da09c4eddd166ec17f38eb1
bibraj96 <blendi.ibraj96@gmail.com>
2020-12-17 13:21:05 -0500

    return json token when user successfully logs in

--- routes/auth.js
+++ routes/auth.js
@@ -42,8 +42,24 @@ router.post('/', [
     if(!isMatch) {
       return res.status(400).json({ msg: 'Invalid Credentials' })
     }
-  } catch(err) {
 
+    const payload = { // With just the id, we can access all of the contacts the user has
+      user: {
+        id: user.id
+      }
+    }
+
+    // Respond with JSON Web Token
+    jwt.sign(payload, config.get('jwtSecret'), {
+      expiresIn: 360000
+    }, (err, token) => {
+      if(err) throw err
+      res.json({ token })
+    })
+
+  } catch(err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
   }
 })
 
