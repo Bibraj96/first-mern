bd4f0eb3412c6396e62f4ed6311a6b41c9c27b85
bibraj96 <blendi.ibraj96@gmail.com>
2020-12-18 10:29:57 -0500

    add async to post contact request

--- routes/contacts.js
+++ routes/contacts.js
@@ -25,7 +25,7 @@ router.get('/', auth, async (req, res) => {
 // @access    Private
 router.post('/', [auth, [
   check('name', 'Name is required').not().isEmpty()
-]], (req, res) => { 
+]], async (req, res) => { 
   const errors = validationResult(req);
   if(!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() })
@@ -44,8 +44,9 @@ router.post('/', [auth, [
 
     const contact = await new Contact.save()
     res.json(contact)
-  } catch (error) {
-    
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
   }
 })
 
