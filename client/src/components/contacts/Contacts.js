e007de258e537ccf4749e3daac4e7608e2e9775f
bibraj96 <blendi.ibraj96@gmail.com>
2020-12-21 13:58:07 -0500

    grab hard coded contacts and map thru displaying name

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -2,13 +2,15 @@ import React, { useContext } from 'react'
 import ContactContext from '../../context/contact/contactContext'
 
 const Contacts = () => {
-  // initialize context
+  // Initialize context so we have access to actiosn associated with this context
   const contactContext = useContext(ContactContext)
-  
+
+  const { contacts } = contactContext;
+
   return (
-    <div>
-      
-    </div>
+    <>
+      {contacts.map(contact => <h3>{contact.name}</h3>)}
+    </>
   )
 }
 
