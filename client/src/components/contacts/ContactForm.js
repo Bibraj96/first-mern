3304dd2d6d504ba2748573e7cd53f1e994b5e63f
bibraj96 <blendi.ibraj96@gmail.com>
2020-12-21 14:44:08 -0500

    set component level state and import useState

---
+++ client/src/components/contacts/ContactForm.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+const ContactForm = () => {
+
+  const [contact, setContact] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    type: 'personal'
+  });
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ContactForm
