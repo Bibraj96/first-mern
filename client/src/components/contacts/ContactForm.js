037d7539e8c69668ea1928220577908f38ae3756
bibraj96 <blendi.ibraj96@gmail.com>
2020-12-22 15:48:06 -0500

    change title and submit button text of form depending on if current is null or not

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -45,7 +45,7 @@ const ContactForm = () => {
 
   return (
     <form onSubmit={onSubmit}>
-      <h2 className="text-primary">Add Contact</h2>
+      <h2 className="text-primary">{current ? 'Edit Contact' : 'Add Contact'}</h2>
       <input 
         type="text" 
         placeholder="Name" 
@@ -85,7 +85,7 @@ const ContactForm = () => {
         /> {' '}
         Professional
       <div>
-        <input type="submit" value="Add Contact" className="btn btn-primary btn-block" />
+        <input type="submit" value={current ? 'Update Contact' : 'Add Contact'} className="btn btn-primary btn-block" />
       </div>
     </form>
   )
