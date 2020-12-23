dfad86e6ba7e390c917c2ddee08a9bebff14f207
bibraj96 <blendi.ibraj96@gmail.com>
2020-12-23 15:20:34 -0500

    add conditional on what to display if filtered is empty or contains a contact

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -6,11 +6,18 @@ const Contacts = () => {
   // Initialize context so we have access to actiosn associated with this context
   const contactContext = useContext(ContactContext)
 
-  const { contacts } = contactContext;
+  const { contacts, filtered } = contactContext;
+
+  if(contacts.length === 0) {
+    return <h4>Please add a contact</h4>
+  }
 
   return (
+    // Check to see if anything is in filtered.
+    // If there is, map through and show contact item
+    // If there isn't, show the contacts
     <>
-      {contacts.map(contact => <ContactItem key={contact.id} contact={contact} />)}
+      {filtered !== null ? filtered.map(contact => (<ContactItem key={contact.id} contact={contact} />)) : contacts.map(contact => <ContactItem key={contact.id} contact={contact} />)}
     </>
   )
 }
