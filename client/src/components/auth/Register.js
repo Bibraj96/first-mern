1f67f61786eb088bdd060cd67a762e6e7c58d983
bibraj96 <blendi.ibraj96@gmail.com>
2020-12-26 13:11:14 -0500

    add form to register

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -8,11 +8,32 @@ const Register = () => {
     password2: ''
   })
 
-  const { name, email, password, password2 }
-  
+  const { name, email, password, password2 } = user;
+
   return (
-    <div>
-      
+    <div className='form-container'>
+      <h1>
+        Account <span className='text-primary'>Register</span>
+      </h1>
+      <form>
+        <div className='form-group'>
+          <label htmlFor='name'>Name</label>
+          <input type='text' name='name' value={name} onChange={onChange} />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='email'>Email</label>
+          <input type='email' name='email' value={email} onChange={onChange} />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='password'>Confirm Password</label>
+          <input type='password' name='password' value={password} onChange={onChange} />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='password2'>Password</label>
+          <input type='password' name='password2' value={password2} onChange={onChange} />
+        </div>
+        <input type='submit' value='Register' className='btn btn-primary btn-block'/>
+      </form>
     </div>
   )
 }
